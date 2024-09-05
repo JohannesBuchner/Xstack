e3ed9b2e40dc16bb0df11a6fb18722f5ffdd5e7b
AstroChensj <JohnnyCsj666@gmail.com>
2024-09-05 11:44:53 +0200

    added bootstrap feature

--- scripts/runXstack.py
+++ scripts/runXstack.py
@@ -21,13 +21,15 @@ from Xstack import Xstack
 #from Xstack.shift_arf import align_arf
 
 # the usual packages
-#import numpy as np
+import numpy as np
 from astropy.io import fits as pyfits
 import os
+import shutil
 import sys
 import argparse
 
 nh_file = Xstack.default_nh_file
+script_path = os.path.abspath(__file__) # the absolute path of runXstack.py
 
 class HelpfulParser(argparse.ArgumentParser):
 	def error(self, message):
@@ -48,7 +50,12 @@ parser.add_argument('flux_energy_lo', type=float, help='lower end of the energy 
 parser.add_argument('flux_energy_hi', type=float, help='upper end of the energy range in keV for computing flux')
 parser.add_argument('arf_scale_method', type=str, default='SHP', help='method to calculate ARF weighting factor for each source: "FLX": assuming all sources have same energy flux (weigh by exposure time), "LMN": assuming all sources have same luminosity (weigh by exposure/dist^2), "SHP": assuming all sources have same spectral shape')
 parser.add_argument('--parametric_rmf', type=bool, default=True, help='method to shift RMF: parametric (gaussian) or not (re-sample response matrices)')
+parser.add_argument('--rm_ene_dsp', action='store_true', help='remove energy dispersion map (the map is used for shifting RMF in parametric mode)')
 parser.add_argument('--num_bkg_groups', type=int, default=10, help='number of background groups')
+# adding bootstrap
+parser.add_argument('--num_bootstrap', type=int, default=-1, help='Whether or not to perform bootstrap experiments. If `num_bootstrap`>0, will generate `num_bootstrap` scripts, which can be later performed by the user with parallel command such as `parallel -j 10 -a bootstrap.sh`. `num_bootstrap` is by default -1, which means that no bootstrap experiments will be performed.')
+parser.add_argument('--pre_bootstrap', type=str, default='bs', help='prefix of all bootstrap files')
+
 
 args = parser.parse_args()
 
@@ -92,33 +99,74 @@ def main():
         arffile_lst.append(arffile)
         rmffile_lst.append(rmffile)
 
+
     if args.num_bkg_groups > len(phafile_lst):
           print('Warning! `Nbkggrp` must be smaller than the number of spectra loaded. `Nbkggrp` is now set to 1.')
           args.num_bkg_groups = 1
 
-    # initialize Xstack
-    data_po = Xstack.XSTACK(
-        phafile_lst=phafile_lst,                       # the PHA files list
-        arffile_lst=arffile_lst,                       # the ARF list
-        rmffile_lst=rmffile_lst,                       # the RMF list
-        z_lst=z_lst,                                   # the redshift list
-        bkgphafile_lst=bkgphafile_lst,                 # the bkg PHA files list
-        nh_lst=nh_lst,                                 # the Galactic absorption list (optional, in units of 1 cm^{-2})
-        srcid_lst=None,                                # the source id list (optional)
-        arfscal_method=args.arf_scale_method,          # the method to calculate ARF weighting factor for each source
-        int_rng=(args.flux_energy_lo, args.flux_energy_hi), # if `arfscal_method`=`SHP`, choose the range to calculate flux
-        rmfsft_method='PAR' if args.parametric_rmf else 'NONPAR', # the RMF shifting method
-        sample_rmf=None,                               # the sample RMF to read input/output energy bin edge (if not specified, the first RMF in `rmffile_lst` will be used)
-        sample_arf=None,                               # the sample ARF to read input/output energy bin edge (if not specified, the first RMF in `rmffile_lst` will be used)
-        nh_file=nh_file,                               # the Galactic absorption profile (absorption factor vs. energy)
-        Nbkggrp=args.num_bkg_groups,                   # the number of background groups to calculate uncertainty of background
-        rm_ene_dsp=True,                               # whether or not to remove the energy dispersion map
-        o_pha_name=args.outsrc,
-        o_bkgpha_name=args.outbkg,
-        o_arf_name=args.outarf,
-        o_rmf_name=args.outrmf,
-    ).run()
-    print(data_po)
+    # Experimental feature: bootstrap (activated by setting num_bootstrap>0)
+    if args.num_bootstrap > 0:
+          shutil.rmtree('%s_filelist'%args.pre_bootstrap,ignore_errors=True)
+          shutil.rmtree('%s_data'%args.pre_bootstrap,ignore_errors=True)
+          os.mkdir('%s_filelist'%args.pre_bootstrap) # make a directory to store the bootstrapped filelists
+          os.mkdir('%s_data'%args.pre_bootstrap) # make a directory to store all bootstrapped stacked PHA, bkgPHA, ARF and RMF files
+          command_lst = []
+          np.random.seed(args.num_bootstrap) # initialize seed
+          for i in range(args.num_bootstrap):
+               idx = str(i).zfill(len(str(args.num_bootstrap)))
+               sampled_idx = np.random.choice(np.arange(len(phafile_lst)),size=len(phafile_lst),replace=True)
+               sampled_phafile_lst = np.array(phafile_lst)[sampled_idx]
+               filelist_txt = '%s_filelist/%s_%s_filelist.txt'%(args.pre_bootstrap,args.pre_bootstrap,idx)
+               with open(filelist_txt,'w') as f:
+                     for j in range(len(sampled_idx)):
+                           f.writelines('%s\n'%sampled_phafile_lst[j])
+               command_lst.append('python '+script_path
+                                  +' '+filelist_txt
+                                  +' '+str(args.flux_energy_lo)
+                                  +' '+str(args.flux_energy_hi)
+                                  +' '+args.arf_scale_method
+                                  +' --outsrc=%s_data/%s_%s_%s'%(args.pre_bootstrap,args.pre_bootstrap,idx,args.outsrc)
+                                  +' --outbkg=%s_data/%s_%s_%s'%(args.pre_bootstrap,args.pre_bootstrap,idx,args.outbkg)
+                                  +' --outrmf=%s_data/%s_%s_%s'%(args.pre_bootstrap,args.pre_bootstrap,idx,args.outrmf)
+                                  +' --outarf=%s_data/%s_%s_%s'%(args.pre_bootstrap,args.pre_bootstrap,idx,args.outarf)
+                                  +' --parametric_rmf='+str(args.parametric_rmf)
+                                  # keep ene_dsp_map, so --rm_ene_dsp is not included
+                                  +' --num_bkg_groups='+str(args.num_bkg_groups)
+                                  +' --num_bootstrap=-1'  # deactivate boostrap in each individual run
+                                  )
+          bs_script = '%s.sh'%args.pre_bootstrap
+          with open(bs_script,'w') as f:
+                for i in range(len(command_lst)):
+                      f.writelines('%s\n'%command_lst[i])
+          # then the user can run `bs_script` by parallel command
+          # e.g. parallel -j 20 -a bs_script
+          # 20: number of cores to be used
+    
+    # Normal case (bootstrap deactivated by setting num_bootstrap<=0)
+    else:   
+        # initialize Xstack and run
+        data = Xstack.XSTACK(
+            phafile_lst=phafile_lst,                       # the PHA files list
+            arffile_lst=arffile_lst,                       # the ARF list
+            rmffile_lst=rmffile_lst,                       # the RMF list
+            z_lst=z_lst,                                   # the redshift list
+            bkgphafile_lst=bkgphafile_lst,                 # the bkg PHA files list
+            nh_lst=nh_lst,                                 # the Galactic absorption list (optional, in units of 1 cm^{-2})
+            srcid_lst=None,                                # the source id list (optional)
+            arfscal_method=args.arf_scale_method,          # the method to calculate ARF weighting factor for each source
+            int_rng=(args.flux_energy_lo, args.flux_energy_hi), # if `arfscal_method`=`SHP`, choose the range to calculate flux
+            rmfsft_method='PAR' if args.parametric_rmf else 'NONPAR', # the RMF shifting method
+            sample_rmf=None,                               # the sample RMF to read input/output energy bin edge (if not specified, the first RMF in `rmffile_lst` will be used)
+            sample_arf=None,                               # the sample ARF to read input/output energy bin edge (if not specified, the first RMF in `rmffile_lst` will be used)
+            nh_file=nh_file,                               # the Galactic absorption profile (absorption factor vs. energy)
+            Nbkggrp=args.num_bkg_groups,                   # the number of background groups to calculate uncertainty of background
+            rm_ene_dsp=args.rm_ene_dsp,                    # whether or not to remove the energy dispersion map
+            o_pha_name=args.outsrc,
+            o_bkgpha_name=args.outbkg,
+            o_arf_name=args.outarf,
+            o_rmf_name=args.outrmf,
+        ).run()
+        print(data)
 
 if __name__ == '__main__':
     main()
