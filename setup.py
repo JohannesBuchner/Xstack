5823028e11baf2765cc6edc8a4419465c68e0b76
Shi-Jiang Chen <JohnnyCsj666@gmail.com>
2025-06-15 22:03:30 +0800

    add version

--- setup.py
+++ setup.py
@@ -1,30 +1,38 @@
 from setuptools import setup, find_packages
+import os
+
+with open("VERSION") as f:
+    lines = f.readlines()
+    version = lines[0].strip()
+    lastupdate = lines[1].strip()
+
+os.system("cp VERSION ./Xstack/")
+os.system("cp VERSION ./Xstack_scripts/")
 
 setup(
-    name='Xstack',
-    version='0.1.0',
-    description='An X-ray Spectral Shifting and Stacking Code',
-    author='Shi-Jiang Chen, Johannes Buchner and Teng Liu',
-    author_email='JohnnyCsj666@gmail.com',
-    url='https://github.com/AstroChensj/Xstack.git',
+    name="Xstack",
+    version=version,
+    description="An X-ray Spectral Shifting and Stacking Code",
+    author="Shi-Jiang Chen, Johannes Buchner and Teng Liu",
+    author_email="JohnnyCsj666@gmail.com",
+    url="https://github.com/AstroChensj/Xstack.git",
     #packages=find_packages(),
-    packages=['Xstack','Xstack_scripts'],
+    packages=["Xstack","Xstack_scripts"],
     install_requires=[
-        'astropy',
-        'numpy',
-        'scipy',
-        'pandas',
-        'tqdm',
-        'numba',
-        'sfdmap',
-        'joblib',
+        "astropy",
+        "numpy",
+        "scipy",
+        "pandas",
+        "tqdm",
+        "numba",
+        "sfdmap",
+        "joblib",
     ],
-    package_data={'': ['**/tbabs_1e20.txt'],
-                  'Xstack': ["fkspec_sh/*.sh"]},
-    #scripts=['scripts/runXstack.py'],
+    package_data={"": ["**/tbabs_1e20.txt"],
+                  "Xstack": ["fkspec_sh/*.sh"]},
     entry_points={
-        'console_scripts': [
-            'runXstack = Xstack_scripts.Xstack_autoscript:main'
+        "console_scripts": [
+            "runXstack = Xstack_scripts.Xstack_autoscript:main"
         ]
     }
 )
