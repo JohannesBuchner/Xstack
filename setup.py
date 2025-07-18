950ee47369b249fcf3bb6c022a74f4f8d4a22513
Shi-Jiang Chen <JohnnyCsj666@gmail.com>
2025-07-18 10:40:04 +0800

    VERSION control

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py as _build_py
 import os
+import shutil
 
 with open("VERSION") as f:
     lines = f.readlines()
     version = lines[0].strip()
     lastupdate = lines[1].strip()
 
-os.system("cp VERSION ./Xstack/")
-os.system("cp VERSION ./Xstack_scripts/")
+class Copy_VERSION(_build_py):
+    def run(self):
+        for pkg in ["Xstack","Xstack_scripts"]:
+            dst = os.path.join(pkg,"VERSION")
+            shutil.copyfile("VERSION",dst)
+        super().run()
 
 setup(
     name="Xstack",
@@ -23,16 +29,21 @@ setup(
         "numpy",
         "scipy",
         "pandas",
+        "matplotlib",
         "tqdm",
         "numba",
         "sfdmap",
         "joblib",
     ],
-    package_data={"": ["**/tbabs_1e20.txt"],
-                  "Xstack": ["fkspec_sh/*.sh"]},
+    package_data={
+        "": ["**/tbabs_1e20.txt"],
+        "Xstack": ["fkspec_sh/*.sh","VERSION"],
+        "Xstack_scripts": ["VERSION"]
+    },
     entry_points={
         "console_scripts": [
-            "runXstack = Xstack_scripts.Xstack_autoscript:main"
+            "runXstack=Xstack_scripts.Xstack_autoscript:main"
         ]
-    }
+    },
+    cmdclass={'build_py': Copy_VERSION},
 )
